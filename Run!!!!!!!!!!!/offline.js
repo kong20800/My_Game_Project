6c3d62d6b7b91d740f3ccf3cab9cc2f18ede446f
Supachet Jaruratchakul <69157656+kong20800@users.noreply.github.com>
2021-10-07 14:21:47 +0700

    First Project

---
+++ Run!!!!!!!!!!!/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1633589907,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/play-sheet0.png",
+		"images/leftbottom-sheet0.png",
+		"images/rightbottom-sheet0.png",
+		"images/upbottom-sheet0.png",
+		"images/replaybottom-sheet0.png",
+		"media/277214__thedweebman__8-bit-powerup-get-something-big.m4a",
+		"media/277214__thedweebman__8-bit-powerup-get-something-big.ogg",
+		"media/melodyloops-preview-calm-morning-2m30s.m4a",
+		"media/melodyloops-preview-calm-morning-2m30s.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
